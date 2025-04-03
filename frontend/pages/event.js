82ba5cc5f358e8d8c6e05e42ab0c6fc8b0f87685
Nickfx9 <mwathikinicholas20@gmail.com>
2025-04-04 02:30:51 +0300

    Create event.js

---
+++ frontend/pages/event.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const EventsPage = () => {
+  return (
+    <div>
+      <h1>Upcoming Events</h1>
+      <p>Check out the latest events happening on AfricaPulse.</p>
+    </div>
+  );
+};
+
+export default EventsPage;
