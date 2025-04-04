7694a158db09666fcd2562db08fa1eb005c4bab9
Nickfx9 <mwathikinicholas20@gmail.com>
2025-04-04 04:25:14 +0300

    Update Header.js

--- frontend/components/Header.js
+++ frontend/components/Header.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import Link from 'next/link';
+import Image from 'next/image';
+import logo from '../public/logo.svg';
 
 const Header = () => {
   return (
     <header>
       <nav>
         <ul>
+          <li>
+            <Link href="/">
+              <a>
+                <Image src={logo} alt="AfricaPulse Logo" width={50} height={50} />
+              </a>
+            </Link>
+          </li>
           <li><Link href="/"><a>Home</a></Link></li>
           <li><Link href="/about"><a>About</a></Link></li>
           <li><Link href="/events"><a>Events</a></Link></li>
